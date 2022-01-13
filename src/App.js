13722ee4710877dbbd498e420ac73618b0eea6c9
maulik202 <maulik202@gmail.com>
2022-01-13 20:44:39 +1100

    [ENTAIN-QA-TECHNICAL-TASK] update API to list Next to Go races
     * adds support for three category of racing
     * lists upcomming races of selected category
     * lists races in assending order based on time with countdown timer

--- src/App.js
+++ src/App.js
@@ -29,6 +29,7 @@ export default class App extends React.Component {
     }, 1000);
   }
 
+  
   // Clear the interval once app is closed/unmounted
   componentWillUnmount() {
     clearInterval(this.interval);
@@ -36,6 +37,7 @@ export default class App extends React.Component {
 
   // Fetch a specified number of next races to jump, sort by time ascending
   // race categories and races >=60 seconds over start time
+  // ${this.state.rowsToFetch}
   getRacingData = () => {
     fetch(`https://api.neds.com.au/rest/v1/racing/?method=nextraces&count=${this.state.rowsToFetch}`, {
       method: 'GET',
@@ -53,33 +55,38 @@ export default class App extends React.Component {
       let newRaces = [];
 
       const races = data.race_summaries;
-
       for (const [key] of Object.entries(races)) {
         const race = races[key];
-        newRaces = newRaces.concat({
-          // TODO: You will have to work with the API payload to determine what data you require
+        const categoryId = race.category_id;
+        const advertisedStart = race.advertised_start.seconds; 
+        const meetingName = race.meeting_name;
+        const raceNumber = race.race_number;
+        newRaces = newRaces.concat({ 
+          advertisedStart, categoryId, meetingName, raceNumber
         });
       }
-
+      
       // Sort races by time to jump and add them to race list
       newRaces.sort((item1, item2) => {
         return item1.advertisedStart - item2.advertisedStart;
       });
-
       // Filter races for only those which are <60 seconds over start time
       let sortedRaceCount = 0;
 
       this.setState({
         unsortedRaces: newRaces,
         sortedRaces: newRaces.filter((value) => {
-          if (sortedRaceCount < 5 && (value.advertisedStart - this.state.time) > -60000) {
-            sortedRaceCount++;
-            return true;
+          // Checks category Id and adds it into sortedRaces
+          if ( this.selectedCategory == null || this.selectedCategory == value.categoryId) {
+            if (sortedRaceCount < 5 && ((value.advertisedStart * 1000) - this.state.time) > -60000) {
+              sortedRaceCount++;
+              return true;
+            }
           }
           return false;
         })
       });
-
+      
       if (sortedRaceCount < 5) {
         this.setState({ rowsToFetch: this.state.rowsToFetch + 1 });
         this.getRacingData();
@@ -87,13 +94,18 @@ export default class App extends React.Component {
     });
   }
 
+  // Sets the category based on button click
+  setCategory = (category) => {
+    this.selectedCategory = category;
+  }
+
   // Format time in XXmin XXs
   getFormattedTime = (rawTime) => {
-    const timeMs = Math.round((rawTime - this.state.time) / 1000);
+    const timeMs = Math.round(((rawTime * 1000) - this.state.time) / 1000);
     const timeMins = Math.round(Math.abs(timeMs) / 60);
     const timeSecs = Math.abs(timeMs) % 60;
-
-    // TODO: Implement your logic to format the display of the race jump time
+    
+    return `${timeMins} Minutes and ${timeSecs} Seconds`;
   }
 
   // Render components
@@ -102,38 +114,36 @@ export default class App extends React.Component {
       <div className="container">
         <div className="buttonContainer">
           <button className="buttonToggle" onClick={() => {
-            // TODO: Populate the state sets with appropriate actions to give each button functionality
+            this.setCategory(null);
             this.setState();
           }}>All Races</button>
         </div>
         <div className="categories">
           <div className="buttonContainer">
             <button className="buttonToggle" onClick={() => {
-              // TODO: Populate the state sets with appropriate actions to give each button functionality
-              this.setState();
+              this.setCategory("9daef0d7-bf3c-4f50-921d-8e818c60fe61");
             }}>Greyhounds</button>
           </div>
           <div className="buttonContainer">
             <button className="buttonToggle" onClick={() => {
-              // TODO: Populate the state sets with appropriate actions to give each button functionality
-              this.setState();
+              this.setCategory("161d9be2-e909-4326-8c2c-35ed71fb460b");
             }}>Harness</button>
           </div>
           <div className="buttonContainer">
             <button className="buttonToggle" onClick={() => {
-              // TODO: Populate the state sets with appropriate actions to give each button functionality
-              this.setState();
-            }}>Thoroughbreds</button>
+             this.setCategory("4a2788f8-e825-4d36-9894-efd4baf1cfae");
+            }}>Horse</button>
           </div>
         </div>
         <div className="list">
           {this.state.sortedRaces.map(item => (
             <ul>
               {/* TODO: Edit the string below to display the Race number, Meeting name and time to jump */}
-              <span className="item">Race X - Meeting - Jumps in X</span>
+              <span className="item"> Race: {item.raceNumber} - Meeting Name: {item.meetingName} - Jumps in: {this.getFormattedTime(item.advertisedStart)}</span>
             </ul>
           ))}
         </div>
+
       </div>
     );
   }
